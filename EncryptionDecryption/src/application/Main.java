10499820b9bdaef814d82d48c5d9ded438e72fe2
surfaceUsed <153752810+surfaceUsed@users.noreply.github.com>
2024-04-24 21:59:46 +0200

    Add files via upload

---
+++ EncryptionDecryption/src/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] a = new String[]{"-mode", "enc", "-data", "Hello there!", "-key", "5", "-alg", "unicode"};
+
+        new Controller(a).runCipher();
+    }
+}
